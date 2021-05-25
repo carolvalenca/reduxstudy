5e5648aa0b6e14486a12f05d4f1fd8e0e12fad55
Caroliny Valença <caroliny.leandro@ccc.ufcg.edu.br>
2021-05-25 15:21:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
+import { connect, useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { clickButton } from './actions';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState()
+  const dispatch = useDispatch()
+  const teste = useSelector(state => state.clickState)
+
+
+  function inputChange(event) {
+    setInputValue(event.target.value)
+    console.log(inputValue)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ paddingTop: '10px' }}>
+      <input type='text' value={inputValue} onChange={inputChange}/>
+        <button onClick={() => dispatch(clickButton(inputValue))}>
+          Click me!
+        </button>
+        <h1>{teste.newValue}</h1>
     </div>
   );
 }
+
 
 export default App;
